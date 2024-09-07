c32eeef8fdbf637e2d465b3ffafd71d619887662
SatyamPrashant <satyamprashant2002@gmail.com>
2024-09-07 16:37:16 +0530

    Add files via upload

---
+++ FoodOrder Project/frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer>
+<p className="text-center">
+     All Copy Rights Reserved by Kiran Gajjala...
+</p>
+    </footer>
+    </>
+  );
+}
